208c2e44833ac9432a225d645b5d841612772b0c
Valeriia295 <sokolsokol295@gmail.com>
2024-07-23 02:11:03 +0200

    part-1

---
+++ module-1.js
@@ -0,0 +1,89 @@
+//? Task 1
+
+// const productName = "Droid"
+// console.log(productName);
+
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+
+//? Task 2
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem = 3500;
+
+//? Task 3
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+//? Task 4
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// console.log(totalPrice);
+
+//? Task 5
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+//? Task 6
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee; //4850;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// console.log(message);
+
+//? Task 7
+
+// function sayHi() {
+//   console.log('Hello, this is my first function!')
+// }
+
+// sayHi()
+
+//? Task 8
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+//? Task 9
+
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));  //52
+// console.log(add(10, 20, 30));  //60
+// console.log(add(5, 10, 15));  //30
+
+//? Task 10
+
+//? Task 11
+//? Task 12
+//? Task 13
+//? Task 14
+//? Task 15
+//? Task 16
+//? Task 17
