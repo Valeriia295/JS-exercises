d479b2a2191f757349fa00a5d9d3a2c235d74ac9
Valeriia295 <sokolsokol295@gmail.com>
2024-07-24 22:49:38 +0200

    Update module-1.js

--- module-1.js
+++ module-1.js
@@ -80,7 +80,17 @@
 
 //? Task 10
 
+// function makeMessage(name, price) {
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// }
+// console.log(makeMessage("Radar", 6150));
+// console.log(makeMessage("Scanner", 3500));
+// console.log(makeMessage("Reactor", 8000));
+// console.log(makeMessage("Engine", 4070));
+
 //? Task 11
+
 //? Task 12
 //? Task 13
 //? Task 14
